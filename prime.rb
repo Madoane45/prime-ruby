3d8857d6363a4c885373d7cf83f5aa86018c2189
Madoane45 <Madoane45@gmail.com>
2021-02-11 14:20:48 -0500

    commit tree 18327976a1c57ea58bb38199a7857066447ce94c

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
+require "pry"
 # Add  code here!
+def prime?(value)
+    if value <= 1 || value == 0 || value == 1
+        return false
+      elsif
+        (2..value - 1).each do |i|
+          if value % i == 0
+          return false
+        end
+        #binding.pry
+      end
+    end
+    true 
+end
